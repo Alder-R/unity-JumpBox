a45bb043574bd8b1d76ee5f13f2ca1ae3754705e
Alder-R <kdh012356@gmail.com>
2024-06-03 13:50:35 +0900

    드롭다운 활용해 플레이어 색 변경

--- Assets/Shop.cs
+++ Assets/Shop.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
     public GameObject ShopPanel;
+    public GameObject Player;
+    public Dropdown ColorDropdown;
+    public Material[] PlayersMat = new Material[3];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +19,18 @@ public class Shop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ShopButtonClick()
     {
         if (ShopPanel.active == false) ShopPanel.SetActive(true);
         else ShopPanel.SetActive(false);
+    }
+
+    public void ColorChange()
+    {
+        int i = ColorDropdown.value;
+        Player.GetComponent<MeshRenderer>().material = PlayersMat[i];
     }
 }
