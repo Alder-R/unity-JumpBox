819894f484e3a37e8ba47ed40a1e4466b60da8e6
Alder-R <kdh012356@gmail.com>
2024-06-03 10:24:44 +0900

    Update HelloCode.cs

--- Assets/HelloCode.cs
+++ Assets/HelloCode.cs
@@ -42,7 +42,7 @@ public class HelloCode : MonoBehaviour      // 교재 예제 모음
         // 배열 예제
         int[] students = new int[5];
         students[0] = 100; students[1] = 90; students[2] = 80; students[3] = 70; students[4] = 60;
-        for (int i = 0; i < 5; i++) Debug.Log($"{i}번 학생의 점수: {students[i]}");
+        for (int i = 0; i < students.Length; i++) Debug.Log($"{i}번 학생의 점수: {students[i]}");
     }
 
     // Update is called once per frame
